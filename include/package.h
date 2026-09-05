843167cd4f46ad0bc45232557b19cea012b89696
ahmed-x86 <ahmdansmr7@gmail.com>
2026-09-05 14:22:43 +0300

    .

--- include/package.h
+++ include/package.h
@@ -10,5 +10,18 @@ struct Package {
     bool installed = false;
 };
 
+struct PackageDependency {
+    QString name;
+    long long sizeBytes = 0;
+};
+
+struct PackageDetails {
+    long long appSizeBytes = 0;
+    QVector<PackageDependency> dependencies;
+    long long totalDepsBytes = 0;
+    long long totalBytes = 0;
+};
+
+QString formatSize(long long bytes);
 QString prettifyName(const QString &raw);
 QString sanitizeName(const QString &name);
