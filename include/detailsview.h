38f8843adc75377c3cacc43aaa4b0601e6e515ec
ahmed-x86 <ahmdansmr7@gmail.com>
2026-09-05 14:22:30 +0300

    .

--- include/detailsview.h
+++ include/detailsview.h
@@ -1,15 +1,18 @@
 #pragma once
 #include "package.h"
+#include "pacmanmanager.h"
 #include <QWidget>
 #include <QLabel>
 #include <QPushButton>
 #include <QHBoxLayout>
+#include <QScrollArea>
+#include <QVBoxLayout>
 
 class DetailsView : public QWidget
 {
     Q_OBJECT
 public:
-    explicit DetailsView(QWidget *parent = nullptr);
+    explicit DetailsView(PacmanManager *pacman, QWidget *parent = nullptr);
 
     void show(const Package &pkg, int accentIdx);
     QLabel *iconLabel() const { return m_iconLabel; }
@@ -20,12 +23,19 @@ signals:
     void installRequested(Package pkg);
     void removeRequested(Package pkg);
 
+private slots:
+    void onDetailsReady(PackageDetails details);
+
 private:
+    PacmanManager *m_pacman;
     QLabel *m_iconLabel;
     QLabel *m_title;
     QLabel *m_publisher;
     QLabel *m_description;
     QHBoxLayout *m_actionLayout;
+    QWidget *m_statsWidget;
+    QVBoxLayout *m_statsLayout;
+    
     Package m_pkg;
     int m_accentIdx = 0;
 };
