0a2bd2c4f76467e83899ec507b611e751ebc9dc3
ahmed-x86 <ahmdansmr7@gmail.com>
2026-09-05 15:38:07 +0300

    .

--- include/theme.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <QString>
-#include <array>
-
-namespace Theme {
-    QString styleSheet();
-    extern const std::array<const char *, 14> kAccentColors;
-    QString accentColor(int idx);
-}
