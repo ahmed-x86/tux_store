dcaf31b47e4b4268332581cbc8eff547bbad4e08
ahmed-x86 <ahmdansmr7@gmail.com>
2026-09-06 17:53:38 +0300

    .

--- include/package.h
+++ include/package.h
@@ -13,13 +13,16 @@ struct Package {
 struct PackageDependency {
     QString name;
     long long sizeBytes = 0;
+    bool installed = false;
 };
 
 struct PackageDetails {
     long long appSizeBytes = 0;
     QVector<PackageDependency> dependencies;
-    long long totalDepsBytes = 0;
-    long long totalBytes = 0;
+    long long installedDepsBytes = 0;   // deps already on disk (won't be downloaded)
+    long long newDepsBytes = 0;         // deps that still need to be fetched
+    long long downloadBytes = 0;        // appSizeBytes + newDepsBytes -> what actually downloads
+    long long totalBytes = 0;           // appSizeBytes + installedDepsBytes + newDepsBytes -> total installed footprint
 };
 
 QString formatSize(long long bytes);
